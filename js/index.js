fc3bb0f37ec039abcd29500194ad541abc0b88c0
n-pretorious <pretoriousndungu@gmail.com>
2020-08-04 16:28:09 +0300

    Add one from only to submit order

--- js/index.js
+++ js/index.js
@@ -12,19 +12,38 @@ let order = []
 
 
 $(document).ready(function () {
+  // set flavour option depending on pizza category
+  $("#category-select").change(function () {
+    let val = $(this).val();
+    if (val == "classic") {
+      $("#flavour-select").html(
+        "<option value='Hawaiian'>Hawaiian</option> <option value='Boerewors'>Boerewors</option> <option value='BBQ Steak'>BBQ Steak</option> <option value='Regina'>Regina</option> <option value='Peri-peri Chicken'>Peri-peri Chicken</option> <option value='Chicken & Mushroom'>Chicken & Mushroom</option> <option value='Chicken Tikka'>Chicken Tikka</option> <option value='Veg Tikka'>Veg Tikka</option>"
+      )
+    } else if (val == "deluxe") {
+      $("#flavour-select").html(
+        "<option value='Chicken & Beef Pepperoni'>Chicken & Beef Pepperoni</option> <option value='Meat Deluxe'>Meat Deluxe</option> <option value='Chicken Macon BBQ'>Chicken Macon BBQ</option> <option value='Chicken Hawaiian'>Chicken Hawaiian</option> <option value='Beef Pepperoni Plus'>Beef Pepperoni Plus</option> <option value='Cheese Burger'>Cheese Burger</option> <option value='Roast Veg & Feta'>Roast Veg & Feta</option> <option value='Veg Feast'>Veg Feast</option> <option value='Spicy Boerwors'>Spicy Boerwors</option>"
+      )
+    } else if (val == "supreme") {
+      $("#flavour-select").html(
+        "<option value='Nyama Feast'>Nyama Fest</option> <option value='Chicken Feast'>Chicken Feast</option>"
+      )
+    }
+  })
+
   $("#pizza").submit(function (event) {
     event.preventDefault()
 
-    let category = $("#pizza .category").text().toLowerCase()
-    let flavour = $("#pizza .card-title").text().toLowerCase()
-    let size = $("#pizza select").val()
-    let quantity = $("#pizza .quantity").val()
+    let category = $("#category-select").val()
+    let flavour = $("#flavour-select").val()
+    let size = $("#size-select").val()
+    let crust = $("#crust-select").val()
+    let quantity = $("#quantity").val()
     let price
 
     let calculatePrice = () => {
-      if (category === "classic pizzas") {
+      if (category === "classic") {
         if (size === "small" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 500 + 100
             return price
           } else {
@@ -32,7 +51,7 @@ $(document).ready(function () {
             return price
           }
         } else if (size === "medium" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 750 + 100
             return price
           } else {
@@ -40,7 +59,7 @@ $(document).ready(function () {
             return price
           }
         } else if (size === "large" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 950 + 100
             return price
           } else {
@@ -50,9 +69,9 @@ $(document).ready(function () {
         } else {
           alert("Kindly pick pizza SIZE: & QTY: for " + flavour)
         }
-      } else if (category === "deluxe pizzas") {
+      } else if (category === "deluxe") {
         if (size === "small" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 550 + 100
             return price
           } else {
@@ -60,7 +79,7 @@ $(document).ready(function () {
             return price
           }
         } else if (size === "medium" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 800 + 100
             return price
           } else {
@@ -68,7 +87,7 @@ $(document).ready(function () {
             return price
           }
         } else if (size === "large" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 1000 + 100
             return price
           } else {
@@ -78,21 +97,21 @@ $(document).ready(function () {
         } else {
           alert("Kindly pick pizza SIZE: & QTY: for " + flavour)
         }
-      } else if (category === "supreme pizzas") {
+      } else if (category === "supreme") {
         if (size === "small" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 600 + 100
           } else {
             price = quantity * 600
           }
         } else if (size === "medium" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 940 + 100
           } else {
             price = quantity * 940
           }
         } else if (size === "large" && quantity >= 1) {
-          if ($('input[type="checkbox"]').is(":checked")) {
+          if ($('#tops').is(":checked")) {
             price = quantity * 1210 + 100
           } else {
             price = quantity * 1210
@@ -117,15 +136,16 @@ $(document).ready(function () {
     if (pizzaOrdered.size != "" && pizzaOrdered.quantity > 0) {
       // Make first letter capital
       pizzaOrdered.flavour = pizzaOrdered.flavour[0].toUpperCase() + pizzaOrdered.flavour.slice(1)
+      crust = crust[0].toUpperCase() + crust.slice(1)
 
       let orderNumber = $('#checkout-ul li').length + 1
 
       // format checkout info depending on whether topping is checked or not
-      let withTopping = $('input[type="checkbox"]').is(":checked") ? '<small class="text-muted">With topping</small>' : '<small class="text-muted"></small>'
+      let withTopping = $('#tops').is(":checked") ? 'with topping' : ''
 
       $(".badge").text(orderNumber)
 
-      $("#checkout-ul").append('<li class="list-group-item d-flex justify-content-between lh-condensed"><div><h6>' + pizzaOrdered.flavour + '<h6>' + withTopping + '</div><span class="text-muted">Ksh. ' + pizzaOrdered.price + '</span></li>')
+      $("#checkout-ul").append('<li class="list-group-item d-flex justify-content-between lh-condensed"><div><h6>' + pizzaOrdered.flavour + '<h6><small class="text-muted">' + crust + ' crust ' + withTopping + '</small></div><span class="text-muted">Ksh. ' + pizzaOrdered.price + '</span></li>')
     }
   })
 })
